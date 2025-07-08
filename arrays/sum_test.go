6176b86c7f775b9774c05e23467856f3e565a4f9
Gerrod Schirtzinger <gschirtzinger@valiantsolutions.com>
2025-07-08 14:21:36 -0400

    generics with arrays

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -2,7 +2,10 @@ package arrays
 
 import (
 	"reflect"
+	"strings"
 	"testing"
+
+	assert "gerrod.com/generics"
 )
 
 func TestSum(t *testing.T) {
@@ -51,4 +54,74 @@ func TestSumAllTails(t *testing.T) {
 		checkSums(t, got, want)
 	})
 
+}
+
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+
+		assert.AssertEqual(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(x, y string) string {
+			return x + y
+		}
+
+		assert.AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
+	})
+}
+
+func TestBadBank(t *testing.T) {
+	var (
+		riya  = Account{Name: "Riya", Balance: 100}
+		chris = Account{Name: "Chris", Balance: 75}
+		adil  = Account{Name: "Adil", Balance: 200}
+
+		transactions = []Transaction{
+			NewTransaction(chris, riya, 100),
+			NewTransaction(adil, chris, 25),
+		}
+	)
+
+	newBalanceFor := func(account Account) float64 {
+		return NewBalanceFor(account, transactions).Balance
+	}
+
+	assert.AssertEqual(t, newBalanceFor(riya), 200)
+	assert.AssertEqual(t, newBalanceFor(chris), 0)
+	assert.AssertEqual(t, newBalanceFor(adil), 175)
+}
+
+func TestFind(t *testing.T) {
+	t.Run("find first even number", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+		firstEvenNumber, found := Find(numbers, func(x int) bool {
+			return x%2 == 0
+		})
+		assert.AssertTrue(t, found)
+		assert.AssertEqual(t, firstEvenNumber, 2)
+	})
+
+	type Person struct {
+		Name string
+	}
+
+	t.Run("Find the best programmer", func(t *testing.T) {
+		people := []Person{
+			Person{Name: "Kent Beck"},
+			Person{Name: "Martin Fowler"},
+			Person{Name: "Chris James"},
+		}
+
+		king, found := Find(people, func(p Person) bool {
+			return strings.Contains(p.Name, "Chris")
+		})
+
+		assert.AssertTrue(t, found)
+		assert.AssertEqual(t, king, Person{Name: "Chris James"})
+	})
 }
